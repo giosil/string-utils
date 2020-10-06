3400df2996e945bb31f99a30e0214f22e8b6601d
Giorgio Silvestris <giorgio.silvestris@gmail.com>
2020-10-06 10:03:28 +0200

    update

--- src/main/java/org/dew/sutil/SUtil.java
+++ src/main/java/org/dew/sutil/SUtil.java
@@ -123,6 +123,325 @@ class SUtil
     return dp[s1.length()][s2.length()];
   }
   
+  public static
+  String toEscapedText(String text, String sDefault)
+  {
+    if(text == null) return sDefault;
+    int iLength = text.length();
+    if(iLength == 0) return "";
+    StringBuffer sb = new StringBuffer(iLength);
+    for(int i = 0; i < iLength; i++) {
+      char c = text.charAt(i);
+      if(c == '<')  sb.append("&lt;");   else
+      if(c == '>')  sb.append("&gt;");   else
+      if(c == '&')  sb.append("&amp;");  else
+      if(c == '"')  sb.append("&quot;"); else
+      if(c == '\'') sb.append("&apos;"); else
+      if(c > 127) {
+        int code = (int) c;
+        sb.append("&#" + code + ";");
+      }
+      else {
+        sb.append(c);
+      }
+    }
+    return sb.toString();
+  }
+  
+  public static
+  String toHTMLText(String text, String sDefault)
+  {
+    if(text == null) return sDefault;
+    int iLength = text.length();
+    if(iLength == 0) return "";
+    StringBuffer sb = new StringBuffer(iLength);
+    for(int i = 0; i < iLength; i++) {
+      char c = text.charAt(i);
+      switch (c) {
+        case    '<':   sb.append("&lt;");     break;
+        case    '>':   sb.append("&gt;");     break;
+        case    '&':   sb.append("&amp;");    break;
+        case '\300':   sb.append("&Agrave;"); break;
+        case '\310':   sb.append("&Egrave;"); break;
+        case '\314':   sb.append("&Igrave;"); break;
+        case '\322':   sb.append("&Ograve;"); break;
+        case '\331':   sb.append("&Ugrave;"); break;
+        case '\301':   sb.append("&Aacute;"); break;
+        case '\311':   sb.append("&Eacute;"); break;
+        case '\315':   sb.append("&Iacute;"); break;
+        case '\323':   sb.append("&Oacute;"); break;
+        case '\332':   sb.append("&Uacute;"); break;
+        case '\340':   sb.append("&agrave;"); break;
+        case '\350':   sb.append("&egrave;"); break;
+        case '\354':   sb.append("&igrave;"); break;
+        case '\362':   sb.append("&ograve;"); break;
+        case '\371':   sb.append("&ugrave;"); break;
+        case '\341':   sb.append("&aacute;"); break;
+        case '\351':   sb.append("&eacute;"); break;
+        case '\355':   sb.append("&iacute;"); break;
+        case '\363':   sb.append("&oacute;"); break;
+        case '\372':   sb.append("&uacute;"); break;
+        case '\252':   sb.append("&ordf;");   break;
+        case '\260':   sb.append("&deg;");    break;
+        case '\u20ac': sb.append("&euro;");   break;
+        default: {
+          if(c < 128) {
+            sb.append(c);
+          }
+          else {
+            int code = (int) c;
+            sb.append("&#" + code + ";");
+          }
+        }
+      }
+    }
+    return sb.toString();
+  }
+  
+  public static
+  String denormalizeText(String sText)
+  {
+    if(sText == null || sText.length() <= 1) return sText;
+    // i < sText.length() - 1 because String s = sText.substring(i, i + 2);
+    boolean boReplaced = false;
+    StringBuffer sb = new StringBuffer();
+    for(int i = 0; i < sText.length() - 1; i++) {
+      char c = sText.charAt(i);
+      String s = sText.substring(i, i + 2);
+      boReplaced = true;
+      if(s.equals("a'")) sb.append('\340'); else
+      if(s.equals("e'")) sb.append('\350'); else
+      if(s.equals("i'")) sb.append('\354'); else
+      if(s.equals("o'")) {
+        if(i > 1 && !sText.substring(i-2,i).equalsIgnoreCase(" p") && !sText.substring(i-2,i).equalsIgnoreCase("\np")) {
+          sb.append('\362');
+        }
+        else {
+          sb.append("o'");
+        }
+      }
+      else
+      if(s.equals("u'")) sb.append('\371'); else
+      if(s.equals("a`")) sb.append('\341'); else
+      if(s.equals("e`")) sb.append('\351'); else
+      if(s.equals("i`")) sb.append('\355'); else
+      if(s.equals("o`")) sb.append('\363'); else
+      if(s.equals("u`")) sb.append('\372'); else
+      if(s.equals("A'")) sb.append('\300'); else
+      if(s.equals("E'")) sb.append('\310'); else
+      if(s.equals("I'")) sb.append('\314'); else
+      if(s.equals("O'")) {
+        if(i > 1 && !sText.substring(i-2,i).equalsIgnoreCase(" p") && !sText.substring(i-2,i).equalsIgnoreCase("\np")) {
+          sb.append('\322');
+        }
+        else {
+          sb.append("O'");
+        }
+      }
+      else
+      if(s.equals("U'")) sb.append('\331'); else
+      if(s.equals("A`")) sb.append('\301'); else
+      if(s.equals("E`")) sb.append('\311'); else
+      if(s.equals("I`")) sb.append('\315'); else
+      if(s.equals("O`")) sb.append('\323'); else
+      if(s.equals("U`")) sb.append('\332'); else {
+        sb.append(c);
+        boReplaced = false;
+      }
+      if(boReplaced) i++;
+      
+    }
+    char cLast = sText.charAt(sText.length() - 1);
+    if(cLast != '\'' && cLast != '`') sb.append(cLast);
+    return sb.toString();
+  }
+  
+  public static
+  String normalizeText(String sText)
+  {
+    if(sText == null || sText.length() == 0) return sText;
+    StringBuffer sb = new StringBuffer();
+    for(int i = 0; i < sText.length(); i++) {
+      char c = sText.charAt(i);
+      if(c == '\340') sb.append("a'"); else
+      if(c == '\350') sb.append("e'"); else
+      if(c == '\354') sb.append("i'"); else
+      if(c == '\362') sb.append("o'"); else
+      if(c == '\371') sb.append("u'"); else
+      if(c == '\341') sb.append("a`"); else
+      if(c == '\351') sb.append("e`"); else
+      if(c == '\355') sb.append("i`"); else
+      if(c == '\363') sb.append("o`"); else
+      if(c == '\372') sb.append("u`"); else
+      if(c == '\300') sb.append("A'"); else
+      if(c == '\310') sb.append("E'"); else
+      if(c == '\314') sb.append("I'"); else
+      if(c == '\322') sb.append("O'"); else
+      if(c == '\331') sb.append("U'"); else
+      if(c == '\301') sb.append("A`"); else
+      if(c == '\311') sb.append("E`"); else
+      if(c == '\315') sb.append("I`"); else
+      if(c == '\323') sb.append("O`"); else
+      if(c == '\332') sb.append("U`"); else
+      if(c > 127) sb.append(" ");else sb.append(c);
+    }
+    return sb.toString();
+  }
+  
+  public static
+  String removeHtmlTags(String sText)
+  {
+    StringBuilder sbResult = new StringBuilder();
+    if(sText == null || sText.length() == 0) {
+      return sbResult.toString();
+    }
+    boolean boIsTag = false;
+    int iTextLength = sText.length();
+    int iStartTag = -1;
+    for(int i = 0; i < iTextLength; i++) {
+      char c = sText.charAt(i);
+      if(c == '<' && i < iTextLength - 1) {
+        char c1 = sText.charAt(i + 1);
+        boIsTag = (Character.isLetter(c1) || c1 == '/') && sText.indexOf('>', i) > 0;
+        if(boIsTag) iStartTag = i;
+      }
+      if(c == '>') {
+        boIsTag = false;
+        if(iStartTag >= 0) {
+          String sTag = sText.substring(iStartTag, i + 1);
+          if(sTag.startsWith("<br") || sTag.startsWith("<BR")) sbResult.append('\n'); else
+          if(sTag.equalsIgnoreCase("</P>")) sbResult.append('\n');
+        }
+      }
+      if(!boIsTag && c != '>') {
+        if(c == '&') {
+          int iEnd = sText.indexOf(';', i);
+          if(iEnd > 0 && iEnd - i <= 8) {
+            String sSeq = sText.substring(i, iEnd + 1);
+            if(sSeq.equalsIgnoreCase("&agrave;")) sbResult.append('\340'); else
+            if(sSeq.equalsIgnoreCase("&egrave;")) sbResult.append('\350'); else
+            if(sSeq.equalsIgnoreCase("&igrave;")) sbResult.append('\354'); else
+            if(sSeq.equalsIgnoreCase("&ograve;")) sbResult.append('\362'); else
+            if(sSeq.equalsIgnoreCase("&ugrave;")) sbResult.append('\371'); else
+            if(sSeq.equalsIgnoreCase("&aacute;")) sbResult.append('\341'); else
+            if(sSeq.equalsIgnoreCase("&eacute;")) sbResult.append('\351'); else
+            if(sSeq.equalsIgnoreCase("&iacute;")) sbResult.append('\355'); else
+            if(sSeq.equalsIgnoreCase("&oacute;")) sbResult.append('\363'); else
+            if(sSeq.equalsIgnoreCase("&uacute;")) sbResult.append('\372'); else
+            if(sSeq.equalsIgnoreCase("&ordf;"))   sbResult.append('\252'); else
+            if(sSeq.equalsIgnoreCase("&deg;"))    sbResult.append('\260'); else
+            if(sSeq.equalsIgnoreCase("&euro;"))   sbResult.append('\u20ac'); else
+            if(sSeq.equalsIgnoreCase("&gt;"))     sbResult.append('>');    else
+            if(sSeq.equalsIgnoreCase("&lt;"))     sbResult.append('<');
+            i = iEnd;
+          }
+          else {
+            sbResult.append(c);
+          }
+        }
+        else {
+          sbResult.append(c);
+        }
+      }
+    }
+    return sbResult.toString();
+  }
+  
+  public static
+  String getQueryStringParValue(String url, String param)
+  {
+    if(url == null || url.length() == 0) return null;
+    int iStartPar = url.indexOf(param + "=");
+    if(iStartPar < 0) return null;
+    int iEndPar = url.indexOf('&', iStartPar);
+    if(iEndPar < 0) iEndPar = url.length();
+    int iStartValue = iStartPar + (param + "=").length();
+    return url.substring(iStartValue, iEndPar);
+  }
+  
+  public static
+  String getJSONVal(String jsonObject, String key, String defaultValue)
+  {
+    if(jsonObject == null || jsonObject.length() == 0 || jsonObject.indexOf('{') < 0) {
+      return defaultValue;
+    }
+    if(key == null || key.length() == 0) {
+      return defaultValue;
+    }
+    // Find key
+    int k = jsonObject.indexOf("\"" + key + "\"");
+    if(k < 0) {
+      return defaultValue;
+    }
+    // Separator key-value
+    int s = jsonObject.indexOf(':', k + key.length() + 2);
+    if(s < 0) {
+      return defaultValue;
+    }
+    // Find end value
+    int  e = -1;
+    // Previous char
+    char cp = '\0';
+    // String delimiter char
+    char cs = '\0';
+    // Is string?
+    boolean jsonString = false;
+    for(int i = s; i < jsonObject.length() - 1; i++) {
+      char c = jsonObject.charAt(i);
+      if(c == '"' && cp != '\\') {
+        if(jsonString) {
+          if(cs == '"') {
+            // End string
+            jsonString = false;
+          }
+        }
+        else {
+          // Start string
+          jsonString = true;
+          cs = '"';
+        }
+      }
+      else if(c == '\'' && cp != '\\') {
+        if(jsonString) {
+          if(cs == '\'') {
+            // End string
+            jsonString = false;
+          }
+        }
+        else {
+          // Start string
+          jsonString = true;
+          cs = '\'';
+        }
+      }
+      else if(c == ',' || c == '}') {
+        if(!jsonString) {
+          // End value found
+          e = i;
+          break;
+        }
+      }
+      // Set previous char
+      cp = c;
+    }
+    if(e < 0) {
+      e = jsonObject.length();
+    }
+    String value = jsonObject.substring(s + 1, e).trim();
+    if(value.equals("null")) {
+      return defaultValue;
+    }
+    // Strip string
+    if(value.startsWith("\"") && value.endsWith("\"")) {
+      value = value.substring(1, value.length() - 1);
+    }
+    else if(value.startsWith("'") && value.endsWith("'")) {
+      value = value.substring(1, value.length() - 1);
+    }
+    // Replace escape characters
+    return value.replace("\\n", "\n").replace("\\t", "\t");
+  }
+  
   private static
   int min(int a, int b, int c) 
   {
